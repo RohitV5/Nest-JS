47e226c4cb8718fbea02161b4c0575eb9cb38995
Rohit Verma <verma.rohit.in@gmail.com>
2021-07-26 23:52:52 +0530

    create task post method

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,20 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './tasks.model';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
 
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks   ();
+  }
 
-    constructor(private tasksService: TasksService) {
-
-    }
-
-    @Get()
-    getAllTasks(): Task[] {
-        return this.tasksService.getAllTasks();
-    }
-
-
-
+  @Post()
+  createTask(@Body() body): Task{
+    console.log('body', body)
+    return body
+  }
 }
