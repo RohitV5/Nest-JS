aa373859c98f80697ac78947d4d37c55dbf1b077
Rohit Verma <verma.rohit.in@gmail.com>
2021-07-29 00:34:11 +0530

    few more validations

--- src/tasks/dto/get-tasks.filter.dto.ts
+++ src/tasks/dto/get-tasks.filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../tasks.model"
 
 export class GetTaskFilterDto{
-    status: TaskStatus;
-    search: string;
+    @IsOptional()
+    @IsEnum(TaskStatus)
+    status?: TaskStatus;
+
+    @IsOptional()
+    @IsString()
+    search?: string;
 }
