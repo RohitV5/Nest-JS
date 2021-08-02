118e8c99d94765982dae8f5eed5b4558b4da2c0d
Rohit Verma <verma.rohit.in@gmail.com>
2021-08-02 15:09:02 +0530

    logging errors

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Param, Delete, Patch, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete, Patch, Query, UseGuards, Logger } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTaskFilterDto } from './dto/get-tasks.filter.dto';
@@ -11,30 +11,16 @@ import { User } from 'src/auth/user.entity';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger('TaskController');
+
   constructor(private tasksService: TasksService) {}
-
-  // @Get()
-  // getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
-  //   // if we have any filters defined, call taskService.getTaskWithFilters
-  //   // otherwise, just get all tasks
-  //   if(Object.keys(filterDto).length){
-  //     return this.tasksService.getTasksWithFilters(filterDto);
-  //   }else{
-  //     return this.tasksService.getAllTasks();
-  //   }
-
-  // }
 
   @Get()
   getTasks(@Query() filterDto: GetTaskFilterDto,@GetUser() user:User ): Promise<Task[]> {
+    this.logger.verbose(`User ${user.username}, Treieving All Task`)
       return this.tasksService.getTasks(filterDto, user);
   }
 
-  
-  // @Get('/:id')
-  // getTaskById(@Param('id') id:string): Task {
-  //     return this.tasksService.getTaskById(id);
-  // }
 
   @Get('/:id')
   getTaskById(@Param('id') id:string, @GetUser() user:User): Promise<Task> {
@@ -42,35 +28,17 @@ export class TasksController {
   }
 
 
-  // @Post()
-  // createTask(@Body() createTaskDto:CreateTaskDto): Task{
-  //   return this.tasksService.createTask(createTaskDto);
-  // }
-
   @Post()
   createTask(@Body() createTaskDto:CreateTaskDto, @GetUser() user:User): Promise<Task>{
     return this.tasksService.createTask(createTaskDto, user);
   }
 
-  // @Delete('/:id')
-  // deleteTask(@Param('id') id:string):void{
-  //   console.log(id)
-  //   return this.tasksService.deleteTask(id);
-  // }
+
   @Delete('/:id')
   deleteTask(@Param('id') id:string, @GetUser() user:User):Promise<void>{
     console.log(id)
     return this.tasksService.deleteTask(id, user);
   }
-
-  // @Patch('/:id')
-  // updateTaskStatus(
-  //   @Param('id') id:string,
-  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
-  // ):Task{
-  //   const {status} = updateTaskStatusDto;
-  //   return this.tasksService.updateTaskStatus(id, status);
-  // }
 
   @Patch('/:id')
   updateTaskStatus(
