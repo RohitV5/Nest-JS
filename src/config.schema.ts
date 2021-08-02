f3006ee0a80655a2458f9cb601406167a95fc718
Rohit Verma <verma.rohit.in@gmail.com>
2021-08-02 19:09:40 +0530

    jwt configurations

--- src/config.schema.ts
+++ src/config.schema.ts
@@ -7,5 +7,6 @@ export const configValidationSchema = Joi.object({
     DB_PORT:Joi.number().default(5432).required(),
     DB_USERNAME:Joi.string().required(),
     DB_PASSWORD:Joi.string().required(),
-    DB_DATABASE: Joi.string().required()
+    DB_DATABASE: Joi.string().required(),
+    JWT_SECRET:Joi.string().required()
 })
